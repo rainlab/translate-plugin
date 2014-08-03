d45ddb5358199c7ba90896138d9e0686a25a9d41
Sam Georges <sam@daftspunk.com>
2014-08-03 12:23:32 +1000

    Fail softly if translate is not built yet

--- classes/Translator.php
+++ classes/Translator.php
@@ -1,7 +1,9 @@
 <?php namespace RainLab\Translate\Classes;
 
 use App;
+use Schema;
 use Session;
+use DbDongle;
 use RainLab\Translate\Models\Locale;
 
 /**
@@ -17,6 +19,8 @@ class Translator
 
     const SESSION_LOCALE = 'rainlab.translate.locale';
 
+    const SESSION_CONFIGURED = 'rainlab.translate.configured';
+
     /**
      * @var string The locale to use on the front end.
      */
@@ -26,10 +30,16 @@ class Translator
      * @var string The default locale if no active is set.
      */
     protected $defaultLocale;
+    
+    /**
+     * @var boolean Determine if translate plugin is configured and ready to be used.
+     */
+    protected $isConfigured;
 
     public function init()
     {
-        $this->activeLocale = $this->defaultLocale = array_get(Locale::getDefault(), 'code', 'en');
+        $this->defaultLocale = $this->isConfigured() ? array_get(Locale::getDefault(), 'code', 'en') : 'en';
+        $this->activeLocale = $this->defaultLocale;
     }
 
     public function setLocale($locale)
@@ -46,6 +56,25 @@ class Translator
     public function getDefaultLocale()
     {
         return $this->defaultLocale;
+    }
+
+    public function isConfigured()
+    {
+        if ($this->isConfigured !== null)
+            return $this->isConfigured;
+
+        if (Session::has(self::SESSION_CONFIGURED)) {
+            $result = true;
+        }
+        elseif (DbDongle::hasDatabase() && Schema::hasTable('rainlab_translate_locales')) {
+            Session::put(self::SESSION_CONFIGURED, true);
+            $result = true;
+        }
+        else {
+            $result = false;
+        }
+
+        return $this->isConfigured = $result;
     }
 
     //
--- routes.php
+++ routes.php
@@ -6,11 +6,15 @@ use RainLab\Translate\Classes\Translator;
 
 App::before(function($request) {
 
+    $translator = Translator::instance();
+    if (!$translator->isConfigured())
+        return;
+
     $locale = Request::segment(1);
     $languages = array_keys(Locale::listEnabled());
 
     if (in_array($locale, $languages)) {
-        Translator::instance()->setLocale($locale);
+        $translator->setLocale($locale);
 
         Route::group(['prefix' => $locale], function() use ($locale) {
             Route::any('{slug}', 'Cms\Classes\Controller@run')->where('slug', '(.*)?');
