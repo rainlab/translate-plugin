880c293fa08ad437f3a51472df97d93ef2dfa01a
Samuel Georges <sam@daftspunk.com>
2016-04-16 08:10:51 +1000

    Rollback: Use CacheHelper for clearing cache Issue is now fixed abd framework cache is flushed with this method

--- controllers/Messages.php
+++ controllers/Messages.php
@@ -2,13 +2,13 @@
 
 use Lang;
 use Flash;
-use Cache;
 use Request;
 use BackendMenu;
 use Backend\Classes\Controller;
 use RainLab\Translate\Models\Message;
 use RainLab\Translate\Models\Locale;
 use RainLab\Translate\Classes\ThemeScanner;
+use System\Helpers\Cache as CacheHelper;
 use System\Classes\SettingsManager;
 
 /**
@@ -46,7 +46,7 @@ class Messages extends Controller
 
     public function onClearCache()
     {
-        Cache::flush();
+        CacheHelper::clear();
 
         Flash::success(Lang::get('rainlab.translate::lang.messages.clear_cache_success'));
     }
