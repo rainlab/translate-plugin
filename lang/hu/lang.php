b29ffe75c9358d6f17ad875ebd10b395c20961e1
Szabó Gergő <gergo85@gmail.com>
2016-08-15 09:17:22 +0200

    Update the Hungarian translate

--- lang/hu/lang.php
+++ lang/hu/lang.php
@@ -3,7 +3,7 @@
 return [
     'plugin' => [
         'name' => 'Fordítás',
-        'description' => 'Többnyelvűvé teszi a weboldalt.',
+        'description' => 'Többnyelvű weboldal létrehozását teszi lehetővé.',
         'tab' => 'Fordítás',
         'manage_locales' => 'Nyelvek kezelése',
         'manage_messages' => 'Szövegek fordítása'
@@ -19,28 +19,35 @@ return [
         'select_label' => 'Nyelv választása',
         'default_suffix' => 'alapértelmezett',
         'unset_default' => 'Már a(z) ":locale" nyelv az alapértelmezett, így nem használható alapértelmezettként.',
+        'delete_default' => 'A(z) ":locale" nyelv az alapértelmezett, így nem törölhető.',
         'disabled_default' => 'A(z) ":locale" nyelv letiltott, így nem állítható be alapértelmezettként.',
         'name' => 'Név',
         'code' => 'Kód',
         'is_default' => 'Alapértelmezett',
         'is_default_help' => 'Az alapértelmezett nyelv a fordítás előtti tartalmat képviseli.',
         'is_enabled' => 'Engedélyezve',
-        'is_enabled_help' => 'A letiltott nyelvek nem lesznek elérhetőek a felhasználói oldalon.',
+        'is_enabled_help' => 'A letiltott nyelvek nem lesznek elérhetőek a látogatói oldalon.',
         'not_available_help' => 'Nincsenek más beállított nyelvek.',
-        'hint_locales' => 'Itt hozhat létre új nyelveket a felhasználói oldal tartalmának lefordításához. Az alapértelmezett nyelv képviseli a fordítás előtti tartalmat.'
+        'hint_locales' => 'Itt hozhat létre új nyelveket a látogatói oldal tartalmának lefordításához. Az alapértelmezett nyelv képviseli a fordítás előtti tartalmat.'
     ],
     'messages' => [
         'title' => 'Szövegek',
         'description' => 'Nyelvi változatok menedzselése.',
         'clear_cache_link' => 'Gyorsítótár kiürítése',
-        'clear_cache_loading' => 'Az alkalmazás gyorsítótár kiürítése...',
-        'clear_cache_success' => 'Sikerült az alkalmazás gyorsítótár kiürítése!',
-        'clear_cache_hint' => 'Böngészőtől függően lehet, hogy a <strong>Gyorsítótár kiürítése</strong> gombra kell kattintania, hogy a módosítások láthatók legyenek a felhasználói oldalon is.',
-        'scan_messages_link' => 'Szöveg keresése',
-        'scan_messages_loading' => 'Új szöveg keresése...',
-        'scan_messages_success' => 'Sikerült a keresés a téma fájljaiban!',
-        'scan_messages_hint' => 'A <strong>Szöveg keresése</strong> gombra kattintva pedig megkeresheti a lefordítandó üzeneteket az aktív téma fájljaiban.',
-        'hint_translate' => 'Itt fordíthatja le a felhasználói oldalon használt üzeneteket. A változtatások automatikusan mentésre kerülnek.',
+        'clear_cache_loading' => 'A weboldal gyorsítótár kiürítése...',
+        'clear_cache_success' => 'Sikerült a weboldal gyorsítótár kiürítése!',
+        'clear_cache_hint' => 'Kattintson a <strong>Gyorsítótár kiürítése</strong> gombra, hogy biztosan láthatóvá váljanak a beírt módosítások a látogatói oldalon is.',
+        'scan_messages_link' => 'Szövegek keresése',
+        'scan_messages_begin_scan' => 'Keresés indítása',
+        'scan_messages_loading' => 'Új szövegek keresése...',
+        'scan_messages_success' => 'Sikerült a szövegek beolvasása!',
+        'scan_messages_hint' => 'A <strong>Szövegek keresése</strong> gombra kattintva pedig beolvashatja a lefordítandó szövegeket.',
+        'scan_messages_process' => 'A folyamat megkisérli beolvasni az aktív témában lévő lefordítandó szövegeket.',
+        'scan_messages_process_limitations' => 'Néhány szöveg nem biztos, hogy azonnal meg fog jelenni a listában.',
+        'scan_messages_purge_label' => 'Szövegek törlése a művelet előtt',
+        'scan_messages_purge_help' => 'Amennyiben bejelöli, úgy minden szöveg törlésre kerül a beolvasást megelőzően.',
+        'scan_messages_purge_confirm' => 'Biztos, hogy töröljük az összes szöveget?',
+        'hint_translate' => 'Itt fordíthatja le a látogatók által elérhető oldalon megjelenő szövegeket. A beírt változtatások automatikusan mentésre kerülnek.',
         'hide_translated' => 'Lefordítottak elrejtése'
     ]
 ];
