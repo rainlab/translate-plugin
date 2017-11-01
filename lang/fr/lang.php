ca2e1aa7c75a0227d4ef580ab78fcd11216144df
Félix Desjardins <FelixINX@users.noreply.github.com>
2017-11-01 11:34:34 -0400

    4 spaces instead of tab

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,7 +2,7 @@
     'plugin' => [
         'name' => 'Traductions',
         'description' => 'Permet de créer des sites Internet multilingues',
-	'tab' => 'Traduction',
+        'tab' => 'Traduction',
         'manage_locales' => 'Manage locales',
         'manage_messages' => 'Manage messages'
     ],
@@ -17,7 +17,7 @@
         'select_label' => 'Sélectionner une langue',
         'default_suffix' => 'défaut',
         'unset_default' => '":locale" est déjà la langue par défaut et ne peut être désactivée',
-	'delete_default' => '":locale" est la valeur par défaut et ne peut pas être supprimé.',
+        'delete_default' => '":locale" est la valeur par défaut et ne peut pas être supprimé.',
         'disabled_default' => '":locale" est désactivé et ne peut être utilisé comme paramètre par défaut.',
         'name' => 'Nom',
         'code' => 'Code',
@@ -27,12 +27,12 @@
         'is_enabled_help' => 'Les langues désactivées ne seront plus disponibles sur le site.',
         'not_available_help' => 'Aucune autre langue n\'est définie.',
         'hint_locales' => 'Vous pouvez ajouter de nouvelles langues et traduire les messages du site. La langue par défaut est celle utilisée pour les contenus avant toute traduction.',
-	'reorder_title' => 'Réorganiser les langues',
+        'reorder_title' => 'Réorganiser les langues',
         'sort_order' => 'Ordre de tri',
     ],
     'messages' => [
         'title' => 'Traduction des Messages',
-	'description' => 'Mettre à jour Messages',
+        'description' => 'Mettre à jour Messages',
         'clear_cache_link' => 'Supprimer le cache',
         'clear_cache_loading' => 'Suppression du cache de l\'application...',
         'clear_cache_success' => 'Le cache de l\'application a été supprimé !',
