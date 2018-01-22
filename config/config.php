8a21ec92d3333c23948dbe1812f3ceae0bc89963
viamage <info@viamage.com>
2018-01-22 22:01:37 +0100

    Make the cache timeout configurable (#335)
    Fixes #334. Credit to @viamage

--- config/config.php
+++ config/config.php
@@ -3,14 +3,27 @@
 return [
     /*
     |--------------------------------------------------------------------------
-    | prefix the default Locale
+    | Prefix the default Locale
     |--------------------------------------------------------------------------
     |
     | Should the default locale be prefixed by the plugin?
     |
-    | ( For now only used in the alternate-hreflang cmponent )
+    | ( For now only used in the alternate-hreflang component )
     |
     */
-    'prefixDefaultLocale' => true
+    'prefixDefaultLocale' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache timeout in minutes
+    |--------------------------------------------------------------------------
+    |
+    | By default all translations are cached for 24 hours (1440 min).
+    | This setting allows to change that period with given amount of minutes.
+    |
+    | ( For example 43200 for 30 days or 525600 for one year )
+    |
+    */
+    'cacheTimeout'        => 1440,
 
 ];
