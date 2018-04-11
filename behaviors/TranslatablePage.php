e53c3f2bd3572d9e4449a9e61b4e41cae6ad2302
Marc Jauvin <marc@r4l.com>
2018-04-10 20:43:11 -0400

    now extends TranslatableBehavior

--- behaviors/TranslatablePage.php
+++ behaviors/TranslatablePage.php
@@ -1,10 +1,7 @@
 <?php namespace RainLab\Translate\Behaviors;
 
 use App;
-use RainLab\Translate\Classes\Translator;
-use October\Rain\Extension\ExtensionBase;
-use ApplicationException;
-use Exception;
+use RainLab\Translate\Classes\TranslatableBehavior;
 
 /**
  * Translatable page model extension
@@ -16,31 +13,17 @@ use Exception;
  *   public $implement = ['@RainLab.Translate.Behaviors.TranslatablePage'];
  *
  */
-class TranslatablePage extends ExtensionBase
+class TranslatablePage extends TranslatableBehavior
 {
-    protected $model;
-
-    protected $attributes = ['title', 'description', 'meta_title', 'meta_description'];
-
-    protected $translatableUseFallback = true;
-
-    protected $translatableContext;
-
-    protected $translatableDefault;
-
-    /**
-     * @var string Default page Attributes.
-     */
-    protected $translatableDefaultAttributes = [];
+    protected $translatableAttributes = ['title', 'description', 'meta_title', 'meta_description'];
 
     public function __construct($model)
     {
         $this->model = $model;
-
         $this->initTranslatableContext();
 
         $this->model->bindEvent('model.afterFetch', function() {
-            $this->translatableDefaultAttributes = $this->getModelAttributes();
+            $this->translatableOriginals = $this->getModelAttributes();
 
             if (!App::runningInBackend()) {
                 $this->rewriteTranslatablePageAttributes();
@@ -51,28 +34,16 @@ class TranslatablePage extends ExtensionBase
     public function getTranslatableAttributes()
     {
         $attributes = [];
-        foreach ($this->attributes as $attribute) {
+        foreach ($this->translatableAttributes as $attribute) {
             $attributes[] = "settings[{$attribute}]";
         }
         return $attributes;
     }
 
-    public function noFallbackLocale()
-    {
-        $this->translatableUseFallback = false;
-
-        return $this->model;
-    }
-
-    protected function setModelAttribute($attr, $value)
-    {
-        $this->model[$attr] = $value;
-    }
-
     protected function getModelAttributes()
     {
         $attributes = [];
-        foreach ($this->attributes as $attr) {
+        foreach ($this->translatableAttributes as $attr) {
             $attributes[$attr] = $this->model[$attr];
         }
         return $attributes;
@@ -80,59 +51,55 @@ class TranslatablePage extends ExtensionBase
 
     public function initTranslatableContext()
     {
-        $translate = Translator::instance();
-        $this->translatableContext = $translate->getLocale();
-        $this->translatableDefault = $translate->getDefaultLocale();
-        $this->translatableDefaultAttributes = $this->getModelAttributes();
+        parent::initTranslatableContext();
+        $this->translatableOriginals = $this->getModelAttributes();
     }
 
     public function rewriteTranslatablePageAttributes($locale = null)
     {
         $locale = $locale ?: $this->translatableContext;
 
-        foreach ($this->attributes as $attr) {
-            $locale_attr = $this->translatableDefaultAttributes[$attr];
+        foreach ($this->translatableAttributes as $attr) {
+            $locale_attr = $this->translatableOriginals[$attr];
 
             if ($locale != $this->translatableDefault) {
                 $locale_attr = $this->getAttributeTranslated($attr, $locale) ?: $locale_attr;
             }
 
-            $this->setModelAttribute($attr, $locale_attr);
+            $this->model[$attr] = $locale_attr;
         }
     }
 
-    public function getAttributeTranslated($attr, $locale)
+    public function getAttributeTranslated($key, $locale = null)
     {
-        if (strpos($attr, '[') !== false)
+        if (strpbrk($key, '[]') !== false) {
             // retrieve attr name within brackets (i.e. settings[title] yields title)
-            $attr = preg_split("/[\[\]]/", $attr)[1];
+            $key = preg_split("/[\[\]]/", $key)[1];
+        }
 
-        $defaults = ($locale == $this->translatableDefault) ? $this->translatableDefaultAttributes[$attr] : null;
+        $defaults = ($locale == $this->translatableDefault) ? $this->translatableOriginals[$key] : null;
 
-        $locale_attr = sprintf('viewBag.locale%s.%s', ucfirst($attr), $locale);
+        $locale_attr = sprintf('viewBag.locale%s.%s', ucfirst($key), $locale);
         return array_get($this->model->attributes, $locale_attr, $defaults);
     }
 
-    public function setAttributeTranslated($attr, $value, $locale)
+    public function setAttributeTranslated($key, $value, $locale = null)
     {
         if ($locale == $this->translatableDefault) {
             return;
         }
 
-        if (strpos($attr, '[') !== false)
+        if (strpbrk($key, '[]') !== false) {
             // retrieve attr name within brackets (i.e. settings[title] yields title)
-            $attr = preg_split("/[\[\]]/", $attr)[1];
+            $key = preg_split("/[\[\]]/", $key)[1];
+        }
 
-        if ($value == $this->translatableDefaultAttributes[$attr]) {
+        if ($value == $this->translatableOriginals[$key]) {
             return;
         }
 
-        /*
-         * The CMS controller will purge attributes just before saving, this
-         * will ensure the attributes are injected after this logic.
-         */
-        $this->model->bindEventOnce('model.beforeSave', function() use ($attr, $value, $locale) {
-            $locale_attr = sprintf('viewBag.locale%s.%s', ucfirst($attr), $locale);
+        $this->model->bindEventOnce('model.beforeSave', function() use ($key, $value, $locale) {
+            $locale_attr = sprintf('viewBag.locale%s.%s', ucfirst($key), $locale);
             if (!$value) {
                 array_forget($this->model->attributes, $locale_attr);
             }
@@ -141,4 +108,8 @@ class TranslatablePage extends ExtensionBase
             }
         });
     }
+
+    // not needed but parent abstract model requires those
+    protected function storeTranslatableData($locale = null) {}
+    protected function loadTranslatableData($locale = null) {}
 }
