126badf90eb9136931cb5c8e684ca0670ac20c97
Samuel Georges <sam@daftspunk.com>
2019-12-29 14:15:24 +1100

    Consistent code fencing + cleanup
    It's OK not to have syntax highlighting for the examples, it promotes the writing of cleaner code when describing the plugin. The October CMS website will auto detect highlighting in most cases, its a shame GitHub does not do this, or there is no method to enable it

--- models/MessageImport.php
+++ models/MessageImport.php
@@ -10,7 +10,7 @@ class MessageImport extends ImportModel
     ];
 
     /**
-     * import the message data from a csv with the following schema:
+     * Import the message data from a csv with the following schema:
      *
      * code  | en    | de    | fr
      * -------------------------------
@@ -18,9 +18,11 @@ class MessageImport extends ImportModel
      * name  | Name  | Name  | Prénom
      * ...
      *
-     * the code column is required and must not be empty.
-     * note: messages with an existing code are not removed/touched if the import doesn't contain this code.
-     * As a result you can incrementally update the messages by just adding the new codes and messages to the csv.
+     * The code column is required and must not be empty.
+     *
+     * Note: Messages with an existing code are not removed/touched if the import
+     * doesn't contain this code. As a result you can incrementally update the
+     * messages by just adding the new codes and messages to the csv.
      *
      * @param $results
      * @param null $sessionKey
@@ -33,14 +35,15 @@ class MessageImport extends ImportModel
             try {
                 if (isset($result[$codeName]) && !empty($result[$codeName])) {
                     $code = $result[$codeName];
-                    // modify result to match the expected message_data schema
+
+                    // Modify result to match the expected message_data schema
                     unset($result[$codeName]);
 
                     $message = Message::firstOrNew(['code' => $code]);
 
-                    // create empty array, if $message is new
+                    // Create empty array, if $message is new
                     $message->message_data = $message->message_data ?: [];
-                    
+
                     if(!isset($message->message_data[Message::DEFAULT_LOCALE])) {
                         $result[Message::DEFAULT_LOCALE] = $code;
                     }
@@ -52,9 +55,11 @@ class MessageImport extends ImportModel
                     } else {
                         $this->logCreated();
                     }
+
                     $message->save();
+
                 } else {
-                    $this->logSkipped($index, 'no code provided');
+                    $this->logSkipped($index, 'No code provided');
                 }
             } catch (\Exception $exception) {
                 $this->logError($index, $exception->getMessage());
