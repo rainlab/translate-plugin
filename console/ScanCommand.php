806539863618f592a3c670b15963e07df2ec94a1
Aurelien Roy <aurelien_roy@outlook.com>
2020-01-10 13:49:29 +0100

    Add console command for scanning messages

--- console/ScanCommand.php
+++ console/ScanCommand.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
 use RainLab\Translate\Classes\ThemeScanner;
 use RainLab\Translate\Models\Message;
 
